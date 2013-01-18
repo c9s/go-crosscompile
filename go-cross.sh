876b4a5d1151dd95b78ebefe04220430d2bd5e8a
c9s <cornelius.howl@gmail.com>
2013-01-19 00:04:40 +0800

    Fix go-cross.sh

---
+++ go-cross.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function cgo-check {
+    if [[ "${GOHOSTOS}" == "freebsd" ]]; then
+        echo 0
+        return
+    fi
+    if [[ "$1" == "${GOHOSTOS}" && "$2" == ${GOHOSTARCH} ]]; then
+        echo 1
+        return
+    fi
+    echo 0
+}
+
+
+eval "$(go env)"
+
+if [[ -z ${GOROOT_-} || -z $GOROOT ]] ; then
+    # GOROOT is not set, use hg to clone go source?
+    echo GOROOT is not set 
+fi
+
+if [[ -z ${GOPLATFORMS-_} || -z $GOPLATFORMS ]] ; then
+    # default platforms
+    # declare -a GOPLATFORMS
+    GOPLATFORMS=('darwin/386' 'darwin/amd64' 'freebsd/386' 'freebsd/amd64' 'freebsd/arm' 'linux/386' 'linux/amd64' 'linux/arm' 'windows/386' 'windows/amd64')
+fi
+
+for platform in "${GOPLATFORMS[@]}"; do
+    local os=${platform%/*}
+    local arch=${platform#*/}
+    echo "Creating function go-$os-$arch..."
+    eval "function go-$os-$arch(){
+    CGO_ENABLED=\$(cgo-check $os $arch) GOOS=$os GOARCH=$arch go \$*
+    }"
+done
+
